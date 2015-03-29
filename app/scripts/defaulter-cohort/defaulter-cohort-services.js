adba7e30b72ac44378092d1cc10563c0ec28586a
jonathandick <jonathan.j.dick@gmail.com>
2015-03-29 09:47:34 +0300

    updating defaulter cohorts module to work offline

--- app/scripts/defaulter-cohort/defaulter-cohort-services.js
+++ app/scripts/defaulter-cohort/defaulter-cohort-services.js
@@ -3,144 +3,164 @@
 /* Services */
 
 var session = sessionStorage;
-var local = localStorage;
-var DEFAULTER_COHORT_CONTEXT  = "https://testserver1.ampath.or.ke";
+var DEFAULTER_COHORT_CONTEXT = "https://testserver1.ampath.or.ke";
 
-var dc = angular.module('defaulterCohort', ['ngResource','ngCookies','openmrsServices','spinner']);
-
-
-dc.factory('DefaulterCohort',['$http','spinnerService',
-  function($http,spinner) {
-      var DefaulterCohort = {};
+var dc = angular.module('defaulterCohort', ['ngResource', 'ngCookies', 'openmrsServices', 'spinner','localStorageServices']);
 
 
-      DefaulterCohort.ping = function() {
-	  $http.get(DEFAULTER_COHORT_CONTEXT).success(function(data) {console.log(data);});
+dc.factory('DefaulterCohort',['$resource',
+  function($resource) {
+    return $resource(
+      (DEFAULTER_COHORT_CONTEXT + '/outreach/ajax_get_defaulter_cohort?defaulter_cohort_uuid=' + uuid),
+      {uuid: '@uuid'},
+      {query: {method: "GET", isArray: false}}
+    )
+  }]);
+
+dc.factory('DefaulterCohortService', ['$http', 'spinnerService','localStorage.utils','Auth',
+  function ($http, spinner, local,Auth) {
+    var DefaulterCohort = {};
+
+    DefaulterCohort.ping = function () {
+      $http.get(DEFAULTER_COHORT_CONTEXT).success(function (data) {
+        console.log(data);
+      });
+    };
+
+    DefaulterCohort.init = function () {
+      var tables = ["defaulter-cohort", "defaulter-cohorts", "outreach-providers"];
+      local.init(tables);
+      DefaulterCohort.getOutreachProviders();
+    };
+
+    DefaulterCohort.get = function (uuid, callback) {
+      spinner.show('waiting');
+      if (uuid === undefined || uuid === "") {
+        uuid = session.getItem("curDefaulterCohortUuid");
+      }
+      else {
+        session.setItem("curDefaulterCohortUuid", uuid);
+      }
+
+      var dc = local.get('defaulter-cohort',uuid);
+      //console.log('dc: ' + dc + ' uuid: ' + uuid);
+      if (dc) {
+        callback(dc);
+        spinner.hide('waiting');
+      }
+      else if (uuid !== undefined && uuid !== "") {
+        DefaulterCohort.get({uuid: patientUuid}, function (data, status, headers) {
+
+          //If the previous cohort was voided, remove from locally saved cohorts
+          if (uuid != data.defaulter_cohort.uuid) local.remove('defaulterCohort',uuid);
+
+          local.set('defaulter-cohort',uuid,data.defaulter_cohort,Auth.getPassword());
+          if (callback) return callback(p);
+          else return p;
+        });
       };
+      spinner.hide('waiting');
 
-      DefaulterCohort.init = function() {
-	  DefaulterCohort.getOutreachProviders();
-      };
+    };
 
-      DefaulterCohort.get = function(uuid,callback) {
-	  spinner.show('waiting');
-	  if(uuid === undefined || uuid === "") {
-	      uuid = session.getItem("curDefaulterCohortUuid");
-	  }
-	  else { session.setItem("curDefaulterCohortUuid",uuid); }
+    DefaulterCohort.getMemberData = function(memberUuids) {
+      var memberUuid,params,nextStartIndex,encounter,encounterUuids;
+      for(var i in memberUuids) {
+        memberUuid = memberUuids[i];
+        params = {startIndex:0, patient:memberUuid};
+        PatientService.get(memberUuid,function(patient) {
+          EncounterService.patientQuery(params,function(encounters) {
+            patient.encounters = encounters;
+            local.set('amrs.patient',patient.uuid,patient,Auth.getPassword())
+          });
+        });
+      }
+    }
 
-	  var dc = session.getItem(uuid);
-	  console.log('dc: ' + dc + ' uuid: ' + uuid);
-	  if(dc) {
-	      callback(JSON.parse(dc));
-	      spinner.hide('waiting');
-	  }
-	  else if (uuid !== undefined && uuid !== "") {
-	      $http.get(DEFAULTER_COHORT_CONTEXT + '/outreach/ajax_get_defaulter_cohort?defaulter_cohort_uuid=' + uuid).success(function(data) {
-		  session.setItem(data.defaulter_cohort.uuid,JSON.stringify(data.defaulter_cohort));
-		  for(var i=0; i<data.defaulter_cohort.patients.length; i++) {
-		      var p = data.defaulter_cohort.patients[i];
-		      session.setItem(p.uuid,JSON.stringify(p));
-		  }
+    DefaulterCohort.getDefaulterCohorts = function (callback) {
+      var dcs = local.getAll("defaulter-cohorts");
+      if (dcs) {
+        console.log("getting defaulter cohorts from local");
+        callback(dcs);
+      }
+      else {
+        console.log("getting defaulter cohorts from server");
 
+        $http.get(DEFAULTER_COHORT_CONTEXT + '/outreach/ajax_get_defaulter_cohorts').success(function (data) {
+          //alert('success getting data from dc server');
+          local.set("defaulter-cohorts", data);
+          callback(data);
+        }).error(function (error, status) {
+          alert('error');
+          alert(error);
+          alert(status);
+        });
+      }
+    };
 
-		  if(uuid != data.defaulter_cohort.uuid) {
-		      local.removeItem("defaulterCohorts");
-		  }
-		  callback(data.defaulter_cohort);
-		  spinner.hide('waiting');
-	      });
-	  }
-      };
-
-      DefaulterCohort.getDefaulterCohorts = function(callback) {
-	  var dcs = local.getItem("defaulterCohorts");
-
-          if(dcs) {
-	      console.log("getting defaulter cohorts from local");
-              callback(JSON.parse(dcs));
+    DefaulterCohort.update = function (uuid, callback) {
+      console.log("updateDefaulterCohort() : updating cohort...");
+      var cohort, numUpdated = 0;
+      if (navigator.onLine) {
+        var url = DEFAULTER_COHORT_CONTEXT + '/outreach/ajax_get_retired_members?defaulter_cohort_uuid=' + uuid;
+        $http.get(url).success(function (retiredPatients) {
+          //The current cohort has been retired and needs to updated with the latest version
+          if (retiredPatients.indexOf("*") != -1) {
+            DefaulterCohort.get(uuid, callback);
           }
           else {
-	      console.log("getting defaulter cohorts from server");
-
-              $http.get(DEFAULTER_COHORT_CONTEXT + '/outreach/ajax_get_defaulter_cohorts').success(function(data) {
-		  //alert('success getting data from dc server');
-                  local.setItem("defaulterCohorts",JSON.stringify(data));
-		  callback(data);
-              }).error(function(error,status) {
-		  alert('error');
-		  alert(error);
-		  alert(status);
-	      });
+            cohort = local.get('defaulter-cohort',uuid,Auth.getPassword());
+            for (var i = 0; i < retiredPatients.length; i++) {
+              var patientUuid = retiredPatients[i];
+              if (patientUuid in cohort.patients) {
+                var p = cohort.patients[patientUuid];
+                if (p.retired == 0) {
+                  cohort.patients[patientUuid].retired = 1;
+                  numUpdated++;
+                }
+              }
+            }
+            local.setItem('defaulter-cohort',uuid,cohort,Auth.getPassword());
+            callback(numUpdated);
           }
-      };
+        });
+      }
+    };
 
-      DefaulterCohort.update = function(uuid,callback) {
-	  console.log("updateDefaulterCohort() : updating cohort...");
-	  var cohort,numUpdated=0;
-	  if(navigator.onLine) {
-	      cohort = JSON.parse(session.getItem(uuid));
-	      console.log('uuid to be update: ' + uuid);
-	      var url = DEFAULTER_COHORT_CONTEXT + '/outreach/ajax_get_retired_members?defaulter_cohort_uuid=' +uuid;
-
-	      $http.get(url).success(function(retiredPatients) {
-		  console.log(retiredPatients);
-		  if(retiredPatients.indexOf("*") != -1) {
-		      session.removeItem(uuid);
-		      local.removeItem("defaulterCohorts");
-		      DefaulterCohort.get(uuid,callback);
-		  }
-		  else {
-		      for(var i=0; i<retiredPatients.length; i++) {
-			  var patientUuid = retiredPatients[i];
-			  if(patientUuid in cohort.patients) {
-			      var p = cohort.patients[patientUuid];
-			      if(p.retired == 0) {
-				  cohort.patients[patientUuid].retired=1;
-				  numUpdated++;
-			      }
-			  }
-		      }
-		      session.setItem(uuid,JSON.stringify(cohort));
-		  }
-              });
-	  }
-	  callback(numUpdated);
-      };
-
-      DefaulterCohort.getNew = function(uuid,callback) {
-	  session.removeItem(uuid);
-	  var url = DEFAULTER_COHORT_CONTEXT + '/outreach/ajax_get_new_defaulter_cohort?defaulter_cohort_uuid=' + uuid;
-	  $http.get(url).success(function(data) {
-	      local.setItem("defaulterCohorts",JSON.stringify(data.defaulter_cohorts));
-              session.setItem(data.defaulter_cohort.uuid,JSON.stringify(data.defaulter_cohort));
-              callback(data.defaulter_cohort);
-	  });
-      };
+    DefaulterCohort.getNew = function (uuid, callback) {
+      local.removeItem('defaulter-cohort',uuid);
+      var url = DEFAULTER_COHORT_CONTEXT + '/outreach/ajax_get_new_defaulter_cohort?defaulter_cohort_uuid=' + uuid;
+      $http.get(url).success(function (data) {
+        local.set("defaulter-cohorts", data.defaulter_cohorts);
+        local.set("defaulter-cohort",data.defaulter_cohort.uuid, data.defaulter_cohort,Auth.getPassword());
+        callback(data.defaulter_cohort);
+      });
+    };
 
 
-      DefaulterCohort.getOutreachProviders = function(callback) {
-	  var url = DEFAULTER_COHORT_CONTEXT + '/outreach/ajax_get_outreach_providers';
-	  var providers = local.getItem('outreach-providers');
+    DefaulterCohort.getOutreachProviders = function (callback) {
+      var url = DEFAULTER_COHORT_CONTEXT + '/outreach/ajax_get_outreach_providers';
+      var providers = local.get('outreach-providers');
 
-	  if(providers === undefined || providers === null) {
-	      console.log('Getting outreach providers from server');
-              $http.get(url).success(function(data) {
-		  console.log('got outreach providers');
-		  local.setItem("outreach-providers",JSON.stringify(data));
-		  providers = data;
-		  if(callback) callback(providers);
-              }).error(function(error) { console.log(error); });
-	  }
-	  else providers = angular.fromJson(providers);
+      if (providers === undefined || providers === null) {
+        console.log('Getting outreach providers from server');
+        $http.get(url).success(function (data) {
+          console.log('got outreach providers');
+          local.setItem("outreach-providers", data);
+          providers = data;
+          if (callback) callback(providers);
+        }).error(function (error) {
+          console.log(error);
+        });
+      }
+      else {
+        if (callback) callback(providers);
+        else return providers;
+      }
+    };
 
 
-	  if(callback) callback(providers);
-	  else return providers;
-      };
-
-
-      return DefaulterCohort;
+    return DefaulterCohort;
 
   }]);
 
--- app/scripts/patient-dashboard/patient-dashboard-directives.js
+++ app/scripts/patient-dashboard/patient-dashboard-directives.js
@@ -3,78 +3,82 @@
 /* Directives */
 
 
-angular.module('patientDashboard',['openmrsServices','flex','utility.widgets'])
-    .directive('encountersPane',['$state','EncounterService','OpenmrsUtilityService',
-      function($state,EncounterService,OpenmrsUtilityService) {
-	  return {
-	    restrict: "E",
-	    scope: {
-		patientUuid : "@",
-	    },
-	    controller : function($scope,$state) {
-		$scope.encounters = [];
-		$scope.busy = false;
-		$scope.nextStartIndex = -1;
+angular.module('patientDashboard', ['openmrsServices', 'flex', 'utility.widgets'])
+  .directive('encountersPane', ['$state', 'EncounterService', 'OpenmrsUtilityService',
+    function ($state, EncounterService, OpenmrsUtilityService) {
+      return {
+        restrict: "E",
+        scope: {
+          patientUuid: "@",
+        },
+        controller: function ($scope, $state) {
+          $scope.encounters = [];
+          $scope.busy = false;
+          $scope.nextStartIndex = -1;
 
-		$scope.showEncounter = function(encUuid,formUuid) {
-		    for(var i in $scope.encounters) {
-			if($scope.encounters[i].uuid === encUuid) { formUuid = $scope.encounters[i].form.uuid;}
-		    }
+          $scope.showEncounter = function (encUuid, formUuid) {
+            for (var i in $scope.encounters) {
+              if ($scope.encounters[i].uuid === encUuid) {
+                formUuid = $scope.encounters[i].form.uuid;
+              }
+            }
 
-		    $state.go('formentry',{encounterUuid:encUuid,patientUuid:$scope.patientUuid,formUuid:formUuid});
-		};
+            $state.go('formentry', {encounterUuid: encUuid, patientUuid: $scope.patientUuid, formUuid: formUuid});
+          };
 
-	    },
+        },
 
-	    link: function(scope, element, attrs) {
-		attrs.$observe('patientUuid',function(newVal,oldVal) {
-		    if(newVal && newVal != "") {
-			scope.busy = false;
-			scope.allDataLoaded = false;
-			scope.nextStartIndex = 0;
-			scope.encounters = [];
-			scope.loadMore();
-		    }
-		});
+        link: function (scope, element, attrs) {
+          attrs.$observe('patientUuid', function (newVal, oldVal) {
+            if (newVal && newVal != "") {
+              scope.busy = false;
+              scope.allDataLoaded = false;
+              scope.nextStartIndex = 0;
+              scope.encounters = [];
+              scope.loadMore();
+            }
+          });
 
-		scope.loadMore = function() {
-		    if(scope.busy === true) return;
-		    scope.busy = true;
+          scope.loadMore = function () {
+            if (scope.busy === true) return;
+            scope.busy = true;
 
-		    var params = {startIndex:scope.nextStartIndex, patient:scope.patientUuid,limit:20};
+            var params = {startIndex: scope.nextStartIndex, patient: scope.patientUuid, limit: 20};
 
-		    EncounterService.patientQuery(params,function(data) {
-			scope.nextStartIndex = OpenmrsUtilityService.getStartIndex(data);
-			for(var e in data.results) {
-			    scope.encounters.push(data.results[e]);
-			}
-			if(scope.nextStartIndex !== undefined){ scope.busy = false;}
-			else scope.allDataLoaded = true;
-		    });
-		};
+            EncounterService.patientQuery(params, function (data) {
+              scope.nextStartIndex = OpenmrsUtilityService.getStartIndex(data);
+              for (var e in data.results) {
+                scope.encounters.push(data.results[e]);
+              }
+              if (scope.nextStartIndex !== undefined) {
+                scope.busy = false;
+              }
+              else scope.allDataLoaded = true;
+            });
+          };
 
-	    },
-	    templateUrl : "views/patient-dashboard/encountersPane.html",
-	}
+        },
+        templateUrl: "views/patient-dashboard/encountersPane.html",
+      }
     }])
-    .directive('formsPane',['FormEntryService',function(FormEntryService) {
-	return {
-	    restrict: "E",
-	    scope: {patientUuid:"@",},
-	    controller : function($scope) {
-		$scope.forms = FormEntryService.getForms();
-	    },
-	    link: function(scope, element, attrs) {
+  .directive('formsPane', ['FormEntryService', function (FormEntryService) {
+    return {
+      restrict: "E",
+      scope: {patientUuid: "@",},
+      controller: function ($scope) {
+        $scope.forms = FormEntryService.getForms();
+      },
+      link: function (scope, element, attrs) {
 
-		/*
-		attrs.$observe('patientUuid',function(newVal,oldVal) {
-		    if(newVal && newVal != "") {
-			scope.patientUuid = newVal;
-		    }
-		});
-		*/
-	    },
-	    templateUrl : "views/patient-dashboard/formsPane.html",
-	}
-    }])
+        /*
+         attrs.$observe('patientUuid',function(newVal,oldVal) {
+         if(newVal && newVal != "") {
+         scope.patientUuid = newVal;
+         }
+         });
+         */
+      },
+      templateUrl: "views/patient-dashboard/formsPane.html",
+    }
+  }])
 ;
